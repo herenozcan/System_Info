629f1f8f98bd4459598bf00c3ffa7d7db240bbf8
herenozcan <herenozcan17@gmail.com>
2022-05-18 16:49:52 +0300

    first

---
+++ system-info.py
@@ -0,0 +1,7 @@
+import platform
+print("Operating System : ", platform.system())
+print("Host-name : " , platform.node())
+print("Machine : ", platform.machine())
+print("Processor : ", platform.processor())
+print("Release : ", platform.release())
+print("Version : ", platform.version())
